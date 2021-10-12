dddeea8fb57b874af0894e3a64cce22eff4c510d
Artm-porjad <artm-porjad@mail.ru>
2021-10-12 18:54:53 +0300

    variables

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert(1)
+const lastName = 'Porjadin'
+let age = 20
+
+const isProgrammer = true
+
+cons _private = 'private'
+cons $ = 'private'
+
+// const if = 'mkef' error
+
+const withNum5 = '5'
+// const 5withnum = 5 error
+
+name = 'Tyoma'
+
+console.log(isProgrammer)
