f830dc9400439c1e9c44cc5033bb6ee790fcc300
Artm-porjad <artm-porjad@mail.ru>
2021-10-12 19:19:50 +0300

    operations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1 переменные
 const firstName = 'Artem'
-const age = 20
+// const age = 20
 
 const isProgrammer = true
 
@@ -14,11 +14,35 @@ const withNum5 = '5'
 
 name = 'Tyoma'
 
-console.log(isProgrammer)
+//console.log(isProgrammer)
 
 // 2 Мутирование
 //console.log('Имя человека: ' + firstName + ', возраст человека ' + age)
 //alert('Имя человека: ' + firstName + ', возраст человека ' + age)
 
-const lastName = prompt('Введите фамилию')
-alert(firstName + ' ' + lastName)
+//const lastName = prompt('Введите фамилию')
+//alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+let currentYear = 2021
+const birthYear = 2001
+
+//const age = currentYear - birthYear
+
+/*
+const a = 10
+const b = 5
+
+let c = 32
+//c = c +a
+c+=a
+
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(++currentYear)
+console.log(--currentYear)
+console.log(c)*/
+
+//
