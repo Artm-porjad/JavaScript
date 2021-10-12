42032482a9dc96eacd858164ac9b2e6c3c78bcd2
Artm-porjad <artm-porjad@mail.ru>
2021-10-12 23:22:12 +0300

    data types

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// 1 переменные
-const firstName = 'Artem'
-// const age = 20
-
-const isProgrammer = true
-
-const _private = 'private'
-const $ = 'private'
-
-// const if = 'mkef' error
-
-const withNum5 = '5'
-// const 5withnum = 5 error
-
-name = 'Tyoma'
+// // 1 переменные
+// const firstName = 'Artem'
+// // const age = 20
+//
+// const isProgrammer = true
+//
+// const _private = 'private'
+// const $ = 'private'
+//
+// // const if = 'mkef' error
+//
+// const withNum5 = '5'
+// // const 5withnum = 5 error
+//
+// name = 'Tyoma'
 
 //console.log(isProgrammer)
 
@@ -45,4 +45,14 @@ console.log(++currentYear)
 console.log(--currentYear)
 console.log(c)*/
 
-//
+// 4 типы данных
+const isProgrammer = true
+const name = 'Artem'
+const age = 21
+let x
+null
+console.log(typeof isProgrammer)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof x)
+console.log(null)
